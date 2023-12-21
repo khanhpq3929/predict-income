99ff8c6cc7a4bcbf3515ea3fd1586a1ac886881f
Khanh Phung <khanh.pq3929@sinhvien.hoasen.edu.vn>
2023-12-21 21:25:29 +0700

    fix: code for deploy

--- app.py
+++ app.py
@@ -8,18 +8,20 @@ import pickle
 
 app = Flask(__name__)
 CORS(app)
-with open('svm_model.pkl', 'rb') as model_file:
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+with open('/home/khanhpq3929/mysite/svm_model.pkl', 'rb') as model_file:
     loaded_svm_model = pickle.load(model_file)
-mapping_folder_path = './categories'
+mapping_folder_path = '/home/khanhpq3929/mysite/categories'
 file_extension = '.txt'
 file_list = [file_name for file_name in os.listdir(mapping_folder_path) if file_name.endswith(file_extension)]
-df_train = pd.read_csv('./data/adult.data')
+df_train = pd.read_csv('/home/khanhpq3929/mysite/adult.data')
 df_train = df_train.drop('income', axis=1).drop('fnlwgt', axis=1)
 for file_name in file_list:
     column = file_name.split('.')[0]
     if column == 'income':
         continue
-    with open(f'./categories/{file_name}', 'r') as file:
+    with open(f'/home/khanhpq3929/mysite/categories/{file_name}', 'r') as file:
         labels = []
         for line in file:
             labels.append(line.strip())
@@ -41,7 +43,7 @@ def encode_data(data):
         column = file_name.split('.')[0]
         if column == 'income':
             continue
-        with open(f'./categories/{file_name}', 'r') as file:
+        with open(f'/home/khanhpq3929/mysite/categories/{file_name}', 'r') as file:
             labels = []
             for line in file:
                 labels.append(line.strip())
@@ -76,6 +78,7 @@ def predict():
     result = postprocess(prediction)
     return jsonify(result)
 
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
